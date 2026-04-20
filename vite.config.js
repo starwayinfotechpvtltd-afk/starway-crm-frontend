84bdbf198c59db6137fa04bf4becf0600af6dad1
starwayinfotechpvtltd-afk <starwayinfotechpvtltd@gmail.com>
2026-04-20 18:04:33 +0530

    Change API proxy target to production URL

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:7000", // Your backend server URL
+        target: "https://crmapi.starwaywebdigital.com",
         changeOrigin: true,
         secure: false,
       },
