1e2e2c18726148357d9114a17fcdc1efb355ef01
ASAD-9900 <mdasad78667@gmail.com>
2026-04-20 17:59:07 +0530

    Changes in src\Caller Pages\Calender.jsx - ASAD

--- src/Caller Pages/Calender.jsx
+++ src/Caller Pages/Calender.jsx
@@ -3,7 +3,7 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Plus } from "lucide-react";
-import "../../../Frontend/src/index.css";
+// import "../../../Frontend/src/index.css";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 const Calendar = () => {
